7a954ed6eeec5d03cc561143cd9042e6b3076c54
Joel Quenneville <joel.quen@gmail.com>
2012-09-12 22:15:51 -0500

    filter current object when object uses the . notation to access attributes

--- lib/ruby_hystorical.rb
+++ lib/ruby_hystorical.rb
@@ -1,6 +1,6 @@
 class RubyHystorical
 
   def self.current(collection)
-    collection.select { |obj| obj[:end_date].nil? }
+    collection.select { |obj| obj.respond_to?(:[]) ? obj[:end_date].nil? : obj.end_date.nil? }
   end
 end
--- spec/ruby_hystorical_spec.rb
+++ spec/ruby_hystorical_spec.rb
@@ -1,18 +1,36 @@
 describe RubyHystorical do
+  class HistoricalObjet
+    attr_accessor :start_date, :end_date
 
-  let(:collection) do
-    [
-      {id: 1, start_date: Date.new(2012, 8, 15), end_date: Date.new(2012, 8, 26)},
-      {id: 2, start_date: Date.new(2012, 8, 27), end_date: Date.new(2012, 9, 4)},
-      {id: 3, start_date: Date.new(2012, 9, 5), end_date: nil}
-    ]
+    def initialize(start_date, end_date)
+      @start_date = start_date
+      @end_date = end_date
+    end
   end
+
+  let(:obj1) { {start_date: Date.new(2012, 8, 15), end_date: Date.new(2012, 8, 26)} }
+  let(:obj2) { {start_date: Date.new(2012, 8, 27), end_date: Date.new(2012, 9, 4)} }
+  let(:obj3) { {start_date: Date.new(2012, 9, 5), end_date: nil} }
+  let(:collection) { [obj1, obj2, obj3] }
+
+
+  let(:obj4) { HistoricalObjet.new(Date.new(2012, 8, 15), Date.new(2012, 8, 26)) }
+  let(:obj5) { HistoricalObjet.new(Date.new(2012, 8, 27), Date.new(2012, 9, 4)) }
+  let(:obj6) { HistoricalObjet.new(Date.new(2012, 9, 5), nil) }
+  let(:collection2) { [obj4, obj5, obj6] }
 
   describe ".current" do
     context "when attributes are accessed via []" do
       it "should return the current object" do
-        RubyHystorical.current(collection).should eq [{id: 3, start_date: Date.new(2012, 9, 5), end_date: nil}]
+        RubyHystorical.current(collection).should eq [obj3]
       end
     end
+
+    context "when attributes are accessed via ." do
+      it "should return the current object" do
+        RubyHystorical.current(collection2).should eq [obj6]
+      end
+    end
+
   end
 end
