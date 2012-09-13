ca2176a1c39907b9e1c599dd91b9afa42a1968cb
Joel Quenneville <joel.quen@gmail.com>
2012-09-12 21:30:02 -0500

    delegate current to subclass

--- lib/hystorical.rb
+++ lib/hystorical.rb
@@ -6,4 +6,8 @@ class Hystorical
     RubyHystorical
     # add conditional to determine which subclass to use once other strategies are used (i.e. ActiveRecord)
   end
+
+  def self.current(collection)
+    delegate_class.current(collection)
+  end
 end
--- spec/hystorical_spec.rb
+++ spec/hystorical_spec.rb
@@ -5,4 +5,13 @@ describe Hystorical do
       Hystorical.delegate_class.should be RubyHystorical
     end
   end
+
+  describe ".current" do
+    it "send current message to delegate class" do
+      Hystorical.stub(:delegate_class).and_return(RubyHystorical)
+      collection = stub
+      RubyHystorical.should_receive(:current).with(collection)
+      Hystorical.current(collection)
+    end
+  end
 end
