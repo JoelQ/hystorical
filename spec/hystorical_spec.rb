f963ca1c09a4917482838aaadb8e2a9099211fb9
Joel Quenneville <joel.quen@gmail.com>
2012-09-12 21:36:40 -0500

    refactor tests

--- spec/hystorical_spec.rb
+++ spec/hystorical_spec.rb
@@ -6,10 +6,10 @@ describe Hystorical do
     end
   end
 
+  let(:collection) { stub }
+  before { Hystorical.stub(:delegate_class).and_return(RubyHystorical) }
   describe ".current" do
     it "send current message to delegate class" do
-      Hystorical.stub(:delegate_class).and_return(RubyHystorical)
-      collection = stub
       RubyHystorical.should_receive(:current).with(collection)
       Hystorical.current(collection)
     end
@@ -17,8 +17,6 @@ describe Hystorical do
 
   describe ".current_on" do
     it "send current_on message to delegate class" do
-      Hystorical.stub(:delegate_class).and_return(RubyHystorical)
-      collection = stub
       date = stub
       RubyHystorical.should_receive(:current_on).with(collection, date)
       Hystorical.current_on(collection, date)
