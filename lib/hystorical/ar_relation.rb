5819c38c0e0504b6224d365086957c6236ab539c
Joel Quenneville <joel.quenneville@collegeplus.org>
2012-09-20 11:07:05 -0500

    add block filter to AR Relation searches

--- lib/hystorical/ar_relation.rb
+++ lib/hystorical/ar_relation.rb
@@ -2,11 +2,13 @@ module Hystorical
   class ARRelation
     class << self
       def current(collection)
-        collection.where(end_date: nil)
+        current_items = collection.where(end_date: nil)
+        block_given? ? current_items.select { |item| yield item } : current_items
       end
 
       def current_on(collection, date)
-        collection.where("? BETWEEN start_date AND end_date", date)
+        current_items = collection.where("? BETWEEN start_date AND end_date", date)
+        block_given? ? current_items.select { |item| yield item } : current_items
       end
     end
   end
