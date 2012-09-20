52683cc24839a8c1b4e68772445b76997463b39f
Joel Quenneville <joel.quenneville@collegeplus.org>
2012-09-20 09:54:54 -0500

    DRY up current and current_on methods by refactoring checks for [] or . accesors into private methods

--- lib/hystorical/ruby_collection.rb
+++ lib/hystorical/ruby_collection.rb
@@ -3,18 +3,26 @@ module Hystorical
     class << self
 
       def current(collection)
-        collection.select { |obj| obj.respond_to?(:[]) ? obj[:end_date].nil? : obj.end_date.nil? }
+        collection.select do |obj|
+          end_date(obj).nil?
+        end
       end
 
       def current_on(collection, date)
         collection.select do |obj|
-          if obj.respond_to?(:[])
-            obj[:start_date] <= date && obj[:end_date] >= date
-          else
-            obj.start_date <= date && obj.end_date >= date
-          end
+          start_date(obj) <= date && end_date(obj) >= date
         end
       end
+
+      private
+      def start_date(obj)
+        obj.respond_to?(:[]) ? obj[:start_date] : obj.start_date
+      end
+
+      def end_date(obj)
+        obj.respond_to?(:[]) ? obj[:end_date] : obj.end_date
+      end
+
     end
   end
 end
